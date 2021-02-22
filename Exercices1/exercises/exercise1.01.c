5514f319daeb8736edb15a7ac0fbc83571b00766
Carlos <arnau.carlos01@gmail.com>
2021-02-22 20:41:15 +0100

    Add files via upload

---
+++ Exercices1/exercises/exercise1.01.c
@@ -0,0 +1,10 @@
+// 01. Write a program that prints "Hello world!" message on screen.
+
+#include <stdio.h>		// Required for: printf(), scanf()
+
+int main() 
+{
+	printf("Hello, World!\n");
+
+	return 0;
+}
